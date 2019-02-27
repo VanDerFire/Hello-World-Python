e970922301eed8dae0efc23b6e302d0b4449eb73
Angl0Firenz3 <39067290+Angl0Firenz3@users.noreply.github.com>
2019-02-27 10:56:35 -0400

    Add files via upload

---
+++ Hello-World-Test.py
@@ -0,0 +1,5 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("Hola Mundo")
+Hola Mundo
+>>> 
